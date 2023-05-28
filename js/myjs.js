f14caa4c0a103794b09804528fc95ceb1a9fd9e1
ElementGC <b20090727@njupt.edu.cn>
2023-05-28 14:29:55 +0800

    Site updated: 2023-05-28 14:29:52

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,6 @@
 position = window.location.pathname
 //修改个人介绍页的页面背景
-if( position == '/about/' ){
+if( position == '/about/self-introduction/' ){
 var p = document.getElementById("page")
 p.setAttribute("style","background:  rgb(250, 250, 250)")
 }
