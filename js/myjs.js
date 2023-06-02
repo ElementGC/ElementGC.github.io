7acc82b9f316743ebb0a25ee6f45aa120ce3ae5f
ElementGC <b20090727@njupt.edu.cn>
2023-06-02 16:09:12 +0800

    Site updated: 2023-06-02 16:09:05

--- js/myjs.js
+++ js/myjs.js
@@ -4,3 +4,7 @@ if( position == '/about/self-introduction/' ){
 var p = document.getElementById("page")
 p.setAttribute("style","background:  rgb(250, 250, 250)")
 }
+// 修改最底部的转载声明的背景色为白色
+ var copyR = document.getElementsByClassName('post-copyright')
+ //因为上面这个获得的是ELements，返回值是一个数组，所以得用[]来表示第一个元素
+copyR[0].setAttribute("style","background:  rgb(250, 250, 250)")
